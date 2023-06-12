d68f7776189f16ff08411d0c387c6156a478a247
KHAIRUL ISLAM <khairulislam38@gmail.com>
2023-06-12 10:27:52 +0600

    [Done] setup logger

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './config';
+import { errorLogger, logger } from './shared/logger';
 
 async function dbConnect() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log('Database connected successfully');
+    logger.info('Database connected successfully');
     app.listen(config.port, () => {
-      console.log(`Server listening on port ${config.port}`);
+      logger.info(`Server listening on port ${config.port}`);
     });
   } catch (error) {
-    console.log('Failed to connect database', error);
+    errorLogger.error('Failed to connect database', error);
   }
 }
 
---
+++ src/shared/logger.ts
@@ -0,0 +1,74 @@
+import { createLogger, format, transports } from 'winston';
+const { combine, timestamp, label, printf } = format;
+import DailyRotateFile from 'winston-daily-rotate-file';
+import path from 'path';
+
+// Custom Log Format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp);
+  return `${date.toDateString()}, ${date.toLocaleTimeString()} [${label}] ${level}: ${message}`;
+});
+
+export const logger = createLogger({
+  level: 'info',
+  format: combine(label({ label: 'P.H.U.' }), timestamp(), myFormat),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new transports.Console(),
+    // new transports.File({
+    //   filename: path.join(
+    //     process.cwd(),
+    //     'logs',
+    //     'winston',
+    //     'successes',
+    //     'phu-%DATE%-success.log'
+    //   ),
+    //   level: 'info',
+    // }),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu-%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH-MM-SS',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '2d',
+    }),
+  ],
+});
+
+export const errorLogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'P.H.U.' }), timestamp(), myFormat),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new transports.Console(),
+    // new transports.File({
+    //   filename: path.join(
+    //     process.cwd(),
+    //     'logs',
+    //     'winston',
+    //     'errors',
+    //     'phu-%DATE%-error.log'
+    //   ),
+    //   level: 'error',
+    // }),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'phu-%DATE%-success.log'
+      ),
+      datePattern: 'YYYY-MM-DD-HH-MM-SS',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '2d',
+    }),
+  ],
+});
